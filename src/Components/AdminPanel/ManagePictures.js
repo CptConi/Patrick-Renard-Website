167fcee7b45355d33ed34ec5295efde3dcf07642
CptConi <nicolas.renard.33@gmail.com>
2020-12-09 09:16:11 +0100

    admin panel update

--- src/Components/AdminPanel/ManagePictures.js
+++ src/Components/AdminPanel/ManagePictures.js
@@ -20,62 +20,69 @@ export default function ManagePictures() {
             <h2>Gestion des Photos:</h2>
 
             <div className='ManagePictures__images-container'>
-                <details>
+                <details open>
                     <summary>Mondes Macros</summary>
-                    {picturesList &&
-                        picturesList
-                            .filter((pic) => pic.category === 'macros')
-                            .map((img) => {
-                                return (
-                                    <Miniature
-                                        image={img.miniaturePath}
-                                        width='500'
-                                        key={img._id}
-                                        id={img._id}
-                                        imgTitle={img.name}
-                                        imgDescription={img.description}
-                                        handleClick={handleDeletePictureClick}
-                                    />
-                                );
-                            })}
+                    <div className='miniature__wrapper'>
+                        {picturesList &&
+                            picturesList
+                                .filter((pic) => pic.category === 'macros')
+                                .map((img) => {
+                                    return (
+                                        <Miniature
+                                            image={img.miniaturePath}
+                                            width='500'
+                                            height='300'
+                                            key={img._id}
+                                            id={img._id}
+                                            imgTitle={img.name}
+                                            imgDescription={img.description}
+                                            handleClick={handleDeletePictureClick}
+                                        />
+                                    );
+                                })}
+                    </div>
                 </details>
-              <details>
+                <details open>
                     <summary>Studio Portraits</summary>
-                    {picturesList &&
-                        picturesList
-                            .filter((pic) => pic.category === 'portraits')
-                            .map((img) => {
-                                return (
-                                    <Miniature
-                                        image={img.miniaturePath}
-                                        width='500'
-                                        key={img._id}
-                                        id={img._id}
-                                        imgTitle={img.name}
-                                        imgDescription={img.description}
-                                        handleClick={handleDeletePictureClick}
-                                    />
-                                );
-                            })}
-              </details>
-                <details>
+                    <div className='miniature__wrapper'>
+                        {picturesList &&
+                            picturesList
+                                .filter((pic) => pic.category === 'portraits')
+                                .map((img) => {
+                                    return (
+                                        <Miniature
+                                            image={img.miniaturePath}
+                                            width='500'
+                                            key={img._id}
+                                            id={img._id}
+                                            imgTitle={img.name}
+                                            imgDescription={img.description}
+                                            handleClick={handleDeletePictureClick}
+                                        />
+                                    );
+                                })}
+                    </div>
+                </details>
+                <details open>
                     <summary>Au Café des Graphistes</summary>
-                    {picturesList &&
-                        picturesList
-                            .filter((pic) => pic.category === 'graphistes')
-                            .map((img) => {
-                                return (
-                                    <Miniature
-                                        image={img.miniaturePath}
-                                        width='500'
-                                        key={img._id}
-                                        id={img._id}
-                                        imgTitle={img.name}
-                                        imgDescription={img.description}
-                                        handleClick={handleDeletePictureClick}
-                                    />
-                                );
-                            })}
+                    <div className='miniature__wrapper'>
+                        {picturesList &&
+                            picturesList
+                                .filter((pic) => pic.category === 'graphistes')
+                                .map((img) => {
+                                    return (
+                                        <Miniature
+                                            image={img.miniaturePath}
+                                            width='500'
+                                            key={img._id}
+                                            id={img._id}
+                                            imgTitle={img.name}
+                                            imgDescription={img.description}
+                                            handleClick={handleDeletePictureClick}
+                                        />
+                                    );
+                                })}
+                    </div>
                 </details>
             </div>
         </div>
