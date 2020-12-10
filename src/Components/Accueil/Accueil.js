ce7aec64affb8819d7341c77db81c0457bfc74aa
CptConi <nicolas.renard.33@gmail.com>
2020-12-10 09:44:40 +0100

    delete unused vars

--- src/Components/Accueil/Accueil.js
+++ src/Components/Accueil/Accueil.js
@@ -1,40 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { getLandingPagePics } from "../../Services/PictureService";
+import { getLandingPagePics } from '../../Services/PictureService';
 import Title from './Text/Title';
 import Section from './Section';
 import sectionText from '../../Services/content';
 
 export default function Accueil() {
-    const [landingPagePictures, setlandingPagePictures] = useState({
-        macros: '',
-        portraits: '',
-        graphistes: '',
-    });
-    
-    const [content, setContent] = useState(sectionText)
+    const [content, setContent] = useState(sectionText);
     console.log(content);
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await getLandingPagePics();
-            const macros = response.data.filter(
-                (pic) => pic.category === 'macros'
-            )[0].halfSizePath
+            const macros = response.data.filter((pic) => pic.category === 'macros')[0].halfSizePath;
             const portraits = response.data.filter((pic) => pic.category === 'portraits')[0]
                 .halfSizePath;
             const graphistes = response.data.filter((pic) => pic.category === 'graphistes')[0]
                 .halfSizePath;
-            const oldData = [...content]
+            const oldData = [...content];
             oldData[0].imagePath = macros;
             oldData[1].imagePath = portraits;
             oldData[2].imagePath = graphistes;
             setContent(oldData);
-
         };
         fetchData();
     }, []);
-
-    
 
     return (
         <div>
