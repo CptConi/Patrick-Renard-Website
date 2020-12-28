8294b8cb7928cdaf179da8fe0d30ef9de725ea9a
CptConi <nicolas.renard.33@gmail.com>
2020-12-28 10:22:41 +0100

    update mongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const filesRoutes = require('./Routes/filesRoutes');
 const uri = process.env.MONGO_URI;
 
 mongoose
-    .connect(uri, { useNewUrlParser: true })
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
