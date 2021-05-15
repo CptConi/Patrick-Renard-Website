d30938a478c4a9e6e51b1e47b1f5a8bf1f74170e
nicolas <nicolas.renard.33@gmail.com>
2021-05-15 17:43:12 +0200

    [BACK] Add backend images pour test sur Digital Ocean

--- src/Services/http-common.js
+++ src/Services/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://15.237.23.153:3001/files',
-    // baseURL: 'http://localhost:3001/files',
+    // baseURL: 'http://15.237.23.153:3001/files',
+    baseURL: 'http://localhost:3001/files',
     headers: {
         'Content-type': 'application/json',
     },
